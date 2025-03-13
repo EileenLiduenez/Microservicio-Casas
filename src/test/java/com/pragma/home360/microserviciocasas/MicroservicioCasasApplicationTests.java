b48d9763d848853bdef27131d2ed922682606183
Eileen Lidueñez <eileenlc0509@gmail.com>
2025-03-13 17:19:04 -0500

    Carga de archivos de proyecto y carpetas

---
+++ src/test/java/com/pragma/home360/microserviciocasas/MicroservicioCasasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.home360.microserviciocasas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioCasasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
